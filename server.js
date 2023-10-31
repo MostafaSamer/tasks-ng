236d17882ccbcf49ba8d2c36b7b0051f47b2750d
root <root@mostafa-pc>
2023-11-01 00:00:04 +0200

    edit vercel.json

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
+const express = require('express');
 const jsonServer = require('json-server');
+const path = require('path');
+
+const app = express();
 const server = jsonServer.create();
-const router = jsonServer.router('db.json'); // Replace 'db.json' with the name of your JSON data file
-const middlewares = jsonServer.defaults();
+const router = jsonServer.router('db.json'); // Replace 'db.json' with your JSON data file
 
-const port = process.env.PORT || 3000; // Define the port where the server will run
+const angularAppPath = path.join(__dirname, 'dist', 'your-angular-app-name'); // Replace with the correct path to your Angular app
 
-server.use(middlewares);
-server.use('/api', router); // Prefix all API routes with '/api'
+const port = process.env.PORT || 3000;
+
+app.use(express.static(angularAppPath));
+
+app.use('/api', router); // Prefix all API routes with '/api'
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(angularAppPath, 'index.html'));
+});
+
+server.use(app);
 
 server.listen(port, () => {
-  console.log(`JSON-Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
